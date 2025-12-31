765bbc8c760add0fa89b9c48bb3529b1d97a9b14
mohammed abo matter <mohammedabomatter@gmail.com>
2025-12-31 15:24:16 +0200

    initial commit

--- lib/features/booking/presentation/widgets/order_status_widget.dart
+++ lib/features/booking/presentation/widgets/order_status_widget.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:serve_home/core/colors/app_color.dart';
 import 'package:serve_home/core/styles/app_style.dart';
+import 'package:serve_home/features/booking/presentation/view_models/booking_view_model.dart';
 
 class OrderStatusWidget extends StatelessWidget {
   const OrderStatusWidget({
@@ -10,236 +12,238 @@ class OrderStatusWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 10,
-      ),
-      child: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 4,
-              offset: Offset(0, 5),
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
+    return Consumer<BookingViewModel>(
+      builder: (context, provBooking, child) =>  Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 10,
         ),
-        height: 650,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 25,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Order Status', style: AppStyle.body15),
-              SizedBox(height: 50),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Color.fromARGB(
-                          255,
-                          219,
-                          252,
-                          231,
-                        ),
-                        child: Icon(
-                          Icons.check,
-                          color: Color.fromARGB(255, 58, 201, 112),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: VerticalDivider(
-                          color: Color(0xff7bf1a8),
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Booking Confirmed',
-                        style: AppStyle.body15,
-                      ),
-                      SizedBox(height: 5),
-                      Text('10:30 Am', style: AppStyle.body15),
-                    ],
-                  ),
-                ],
-              ),
-    
-              SizedBox(height: 25),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Color.fromARGB(
-                          255,
-                          219,
-                          252,
-                          231,
-                        ),
-                        child: Icon(
-                          Icons.check,
-                          color: Color.fromARGB(255, 58, 201, 112),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: VerticalDivider(
-                          color: Color(0xff7bf1a8),
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Professional Assigned',
-                        style: AppStyle.body15,
-                      ),
-                      SizedBox(height: 5),
-                      Text('10:45 Am', style: AppStyle.body15),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: AppColor.filledTextField,
-                        child: Icon(
-                          Icons.circle,
-                          size: 13,
-                          color: Colors.grey.shade400,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: VerticalDivider(
-                          color: Colors.grey.shade400,
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'On the Way',
-                        style: AppStyle.body15.copyWith(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Expected 10:45 Am',
-                        style: AppStyle.body15.copyWith(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: AppColor.filledTextField,
-                        child: Icon(
-                          Icons.circle,
-                          size: 13,
-                          color: Colors.grey.shade400,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: VerticalDivider(
-                          color: Colors.grey.shade400,
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Service in Progress',
-                        style: AppStyle.body15.copyWith(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: AppColor.filledTextField,
-                        child: Icon(
-                          Icons.circle,
-                          size: 13,
-                          color: Colors.grey.shade400,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: VerticalDivider(
-                          color: Colors.grey.shade400,
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Completed',
-                        style: AppStyle.body15.copyWith(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(0, 5),
               ),
             ],
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          height: 650,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 25,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Order Status', style: AppStyle.body15),
+                SizedBox(height: 50),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color.fromARGB(
+                            255,
+                            219,
+                            252,
+                            231,
+                          ),
+                          child: Icon(
+                            Icons.check,
+                            color: Color.fromARGB(255, 58, 201, 112),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Color(0xff7bf1a8),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Booking Confirmed',
+                          style: AppStyle.body15,
+                        ),
+                        SizedBox(height: 5),
+                        Text(provBooking.selectedTime, style: AppStyle.body15),
+                      ],
+                    ),
+                  ],
+                ),
+      
+                SizedBox(height: 25),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color.fromARGB(
+                            255,
+                            219,
+                            252,
+                            231,
+                          ),
+                          child: Icon(
+                            Icons.check,
+                            color: Color.fromARGB(255, 58, 201, 112),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Color(0xff7bf1a8),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Professional Assigned',
+                          style: AppStyle.body15,
+                        ),
+                        SizedBox(height: 5),
+                        Text(provBooking.selectedTime, style: AppStyle.body15),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: AppColor.filledTextField,
+                          child: Icon(
+                            Icons.circle,
+                            size: 13,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Colors.grey.shade400,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'On the Way',
+                          style: AppStyle.body15.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          'Expected 10:45 Am',
+                          style: AppStyle.body15.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: AppColor.filledTextField,
+                          child: Icon(
+                            Icons.circle,
+                            size: 13,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Colors.grey.shade400,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Service in Progress',
+                          style: AppStyle.body15.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: AppColor.filledTextField,
+                          child: Icon(
+                            Icons.circle,
+                            size: 13,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          child: VerticalDivider(
+                            color: Colors.grey.shade400,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Completed',
+                          style: AppStyle.body15.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/booking/presentation/widgets/service_details_widget.dart
+++ lib/features/booking/presentation/widgets/service_details_widget.dart
@@ -1,6 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:serve_home/core/styles/app_style.dart';
+import 'package:serve_home/features/booking/presentation/view_models/booking_view_model.dart';
+import 'package:serve_home/features/booking/presentation/view_models/location_view_model.dart';
+import 'package:serve_home/features/services/presentation/view_models/service_view_model.dart';
 
 class ServiceDetailsWidget extends StatelessWidget {
   const ServiceDetailsWidget({
@@ -9,74 +13,76 @@ class ServiceDetailsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 10,
-      ),
-      child: Container(
-        height: 250,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 4,
-              offset: Offset(0, 5),
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
+    return Consumer3<BookingViewModel , ServiceViewModel , LocationViewModel>(
+    builder: (context, provBooking,provService ,provLocation,   child) =>  Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 10,
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 25,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Service details', style: AppStyle.body15),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Service', style: AppStyle.body15),
-                  Text('House Cleaning', style: AppStyle.body15),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Date&Time', style: AppStyle.body15),
-                  Text(
-                    '5/10/2025 at 9:00 Am',
-                    style: AppStyle.body15,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Address', style: AppStyle.body15),
-                  Text('Gaza', style: AppStyle.body15),
-                ],
-              ),
-              SizedBox(height: 10),
-    
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Payment Method ', style: AppStyle.body15),
-                  Text(
-                    'Crediat / Debit Card',
-                    style: AppStyle.body15,
-                  ),
-                ],
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(0, 5),
               ),
             ],
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 25,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Service details', style: AppStyle.body19),
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(  'Service',style: AppStyle.body16),
+                    Text(provService.selectedService!.name , style: AppStyle.body15),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Date&Time', style: AppStyle.body16),
+                    Text(
+                      '${provBooking.selectedDate} at ${provBooking.selectedTime}',
+                      style: AppStyle.body15,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Address', style: AppStyle.body16),
+                    Text(provLocation.address, style: AppStyle.body15),
+                  ],
+                ),
+                SizedBox(height: 10),
+      
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Payment Method ', style: AppStyle.body16),
+                    Text(
+                      provBooking.selectedPaymentMethod,
+                      style: AppStyle.body15,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
